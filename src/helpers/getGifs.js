9f0a11565ba99b1bb49651caa73f0fdb92f13046
Gino Facundo Massoni <facundogino@outlook.com>
2021-01-15 14:26:15 -0300

    third commit

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs=async(category)=>{
-    const url=`http://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=5&api_key=B7hV9fziZvOvD9GoBfy2kXtlFegBbXfT`;
+    const url=`https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=5&api_key=B7hV9fziZvOvD9GoBfy2kXtlFegBbXfT`;
     const resp=await fetch(url);
     const {data}=await resp.json();
     const gifs=data.map(img=>{
